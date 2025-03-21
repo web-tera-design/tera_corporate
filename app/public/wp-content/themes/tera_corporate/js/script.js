2962f85ec0bfffeffada61e7dff78f187da888cd
テラ <terakawayusuke@gmail.com>
2025-03-21 21:10:06 +0900

    swiperの基礎スニペット構築

--- app/public/wp-content/themes/tera_corporate/js/script.js
+++ app/public/wp-content/themes/tera_corporate/js/script.js
@@ -1,3 +1,5 @@
+
+// ドロワー
 document.addEventListener('DOMContentLoaded', () => {
     const drawer = document.querySelector('.drawer');
     const drawerIcon = document.querySelector('.drawer-icon');
@@ -76,6 +78,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+
+  // 画面サイズ375px以下はいい感じに縮小
   document.addEventListener("DOMContentLoaded", () => {
     function scaleContent() {
         const minWidth = 375;
@@ -89,7 +93,84 @@ document.addEventListener('DOMContentLoaded', () => {
             document.body.style.width = ""; // デフォルトの幅に戻す
         }
     }
-  
     scaleContent();
     window.addEventListener("resize", scaleContent);
   });
+
+ const cardSwiper = new Swiper('.card__swiper', { //swiperの名前
+   //切り替えのモーション
+   speed: 3000, //表示切り替えのスピード
+   effect: "slide", //切り替えのmotion (※1)
+   allowTouchMove: true, // スワイプで表示の切り替えを有効に
+ 
+   //最後→最初に戻るループ再生を有効に
+   loop: true,
+   //自動スライドについて
+   autoplay: { 
+     delay: 3000, //何秒ごとにスライドを動かすか
+     stopOnLastSlide: false, //最後のスライドで自動再生を終了させるか
+     disableOnInteraction: true, //ユーザーの操作時に止める
+     reverseDirection: false, //自動再生を逆向きにする
+   },
+ 
+    //表示について
+   centeredSlides: true, //中央寄せにする
+   slidesPerView: 1, 
+   spaceBetween: 0,
+ 
+   //ページネーション
+   pagination: {
+     el: ".swiper-pagination", //paginationのclass
+     clickable: true, //クリックでの切り替えを有効に
+     type: "bullets" //paginationのタイプ (※2)
+   },
+   //ナビゲーション
+   navigation: {
+     prevEl: ".swiper-button-prev", //戻るボタンのclass
+     nextEl: ".swiper-button-next" //進むボタンのclass
+   },
+   //スクロールバー
+   scrollbar: { //スクロールバーを表示したいとき
+     el: ".swiper-scrollbar", //スクロールバーのclass
+     hide: true, //操作時のときのみ表示
+     draggable: true //スクロールバーを直接表示できるようにする
+   },
+ 
+   //ブレイクポイントによって変える
+   // breakpoints: { 
+   //   768: {
+   //     slidesPerView: 1.2,
+   //     spaceBetween: 15,
+   //   },
+   //   1500: {
+   //     slidesPerView: 3,
+   //     spaceBetween: 40,
+   //   },
+   // }
+ });
+ 
+ /* =================================================== 
+ ※1 effectについて
+ slide：左から次のスライドが流れてくる
+ fade：次のスライドがふわっと表示
+ ■ fadeの場合は下記を記述
+   fadeEffect: {
+     crossFade: true
+   },
+ cube：スライドが立方体になり、3D回転を繰り返す
+ coverflow：写真やアルバムジャケットをめくるようなアニメーション
+ flip：平面が回転するようなアニメーション
+ cards：カードを順番にみていくようなアニメーション
+ creative：カスタマイズしたアニメーションを使うときに使用します
+ 
+ =======================================================
+ ※2 paginationのタイプ
+ bullets：スライド枚数と同じ数のドットが表示
+ fraction：分数で表示（例：1 / 3）
+ progressbar：スライドの進捗に応じてプログレスバーが伸びる
+ custom：自由にカスタマイズ
+ 
+ =====================================================*/
+
+
+
