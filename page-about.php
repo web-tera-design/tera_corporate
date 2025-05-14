51c04a6262a39598a327739c3d8e29947c938536
テラ <terakawayusuke@gmail.com>
2025-05-14 13:38:39 +0900

    PHpファイル分割開始

---
+++ page-about.php
@@ -0,0 +1,160 @@
+<?php get_header(); ?>
+
+    <section class="c-mv">
+      <div class="c-mv__inner l-section__inner">
+        <div class="c-mv__bg-image">
+          <hgroup class="c-mv__bg-text">
+            <h2 class="c-mv__bg-text--main">当院について</h2>
+            <p class="c-mv__bg-text--sub">ABOUT OUR CLINIC</p>
+          </hgroup>
+        </div>
+        <nav class="l-breadcrumbs" aria-label="パンくずリスト">
+          <ol class="l-breadcrumb__list">
+            <li class="l-breadcrumb__item">
+              <a class="l-breadcrumb__link" href="">ホーム</a>
+            </li>
+            <li class="l-breadcrumb__item">
+              <span class="l-breadcrumb__current">当院について</span>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </section>
+
+    <div class="c-concept__heading-content p-concept__heading-content">
+      <h2 class="c-heading">ポリシーと特徴</h2>
+    </div>
+    <section class="c-concept p-about-concept">
+      <div class="c-concept__inner">
+        <div class="c-concept__container p-about-concept__container">
+          <div class="c-concept__content">
+            <hgroup class="c-concept__heading-group">
+              <p class="c-concept__subheading">POLICY</p>
+              <h2 class="c-concept__heading">
+                コミュニケーションから始まる最適な医療提供
+              </h2>
+            </hgroup>
+            <p class="c-concept__text">
+              当院ではまず患者様から詳しくお話を伺います。<br />
+              難しい言葉は使わず、実際に感じている小さな違和感からあらゆる可能性を考え、最適な治療方法をご提案いたします。
+            </p>
+            <p class="c-concept__text">
+              「どこよりも本音で話せる歯医者さん」を目指し、スタッフ一同、「人間力の向上」にも勤めております。
+            </p>
+          </div>
+          <div class="c-concept__image">
+            <img
+              src="img/concept_img.webp"
+              alt="治療中の画像"
+              width="1280"
+              height="876"
+            />
+          </div>
+        </div>
+        <div class="c-concept__bg-image">
+          <img src="img/concept-bg.svg" alt="" width="1310" height="726" />
+        </div>
+      </div>
+    </section>
+
+    <section class="c-concept">
+      <div class="c-concept__inner">
+        <div class="c-concept__container c-concept--row">
+          <div class="c-concept__content">
+            <hgroup class="c-concept__heading-group">
+              <p class="c-concept__subheading">FEATURE</p>
+              <h2 class="c-concept__heading">
+                「医療技術の追求」と<br />「通いやすさ」
+              </h2>
+            </hgroup>
+            <p class="c-concept__text">
+              歯の治療において、小さな違和感は大きなストレスにつながります。私たちは常に快適な歯科医療技術の研究を行っております。
+              また、「通いやすさ」も医院選びの重要なポイントと考え、2019年のリニューアルを期に更に駅の近くへ場所を移しました。
+            </p>
+            <p class="c-concept__text">
+              朝から夜までお仕事をされている方のために診療時間を見直し、平日でもご利用いただけるようにいたしました。
+            </p>
+          </div>
+          <div class="c-concept__image c-concept__image--row">
+            <img
+              src="img/concept_img.webp"
+              alt="治療中の画像"
+              width="1280"
+              height="876"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="p-about__medical">
+      <div class="p-about-medical__inner l-section__inner">
+        <div class="p-about-medical-headnig">
+          <h2 class="c-heading">院内の様子</h2>
+        </div>
+        <div class="p-about-medical__cards">
+          <div class="p-about-medical__card">
+            <div class="p-about-medical__image">
+              <img
+                src="img/about/medical/pc/about-medical-pc-01.webp"
+                alt="院内の様子"
+                width="634"
+                height="634"
+              />
+            </div>
+          </div>
+          <div class="p-about-medical__card">
+            <div class="p-about-medical__image">
+              <img
+                src="img/about/medical/pc/about-medical-pc-02.webp"
+                alt="院内の様子"
+                width="634"
+                height="634"
+              />
+            </div>
+          </div>
+          <div class="p-about-medical__card">
+            <div class="p-about-medical__image">
+              <img
+                src="img/about/medical/pc/about-medical-pc-03.webp"
+                alt="院内の様子"
+                width="634"
+                height="634"
+              />
+            </div>
+          </div>
+          <div class="p-about-medical__card">
+            <div class="p-about-medical__image">
+              <img
+                src="img/about/medical/pc/about-medical-pc-04.webp"
+                alt="院内の様子"
+                width="634"
+                height="634"
+              />
+            </div>
+          </div>
+          <div class="p-about-medical__card">
+            <div class="p-about-medical__image">
+              <img
+                src="img/about/medical/pc/about-medical-pc-05.webp"
+                alt="院内の様子"
+                width="634"
+                height="634"
+              />
+            </div>
+          </div>
+          <div class="p-about-medical__card">
+            <div class="p-about-medical__image">
+              <img
+                src="img/about/medical/pc/about-medical-pc-06.webp"
+                alt="院内の様子"
+                width="634"
+                height="634"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+<?php get_footer(); ?>
---
+++ sidebar.php
@@ -0,0 +1,212 @@
+<aside class="c-sidebar">
+          <div class="c-sidebar__inner l-section__inner">
+            <div class="c-sidebar__container">
+              <div class="c-sidebar__clinic">
+                <div class="c-sidebar__heading">
+                  <img
+                    src="img/blog/sidebaer/pc/blog-sidebaer-pc-03.svg"
+                    alt=""
+                  />
+                  <h2 class="c-sidebar__heading-title">クリニックの紹介</h2>
+                </div>
+                <div class="c-sidebar__clinic-image">
+                  <img
+                    src="img/blog/sidebaer/pc/blog-sidebaer-pc-02.webp"
+                    alt=""
+                    width="670"
+                    height="420"
+                  />
+                </div>
+                <div class="c-sidebar__clinic-text-items">
+                  <h3 class="c-sidebar__clinic-subtitle">
+                    みなみ歯科クリニック
+                  </h3>
+                  <p class="c-sidebar__clinic-subtext">
+                    お子様からご高齢の方まで、快適な空間で治療が受けられる場を作り、地域医療に貢献しきたいと考えております。
+                  </p>
+                  <a href="" class="c-sidebar__clinic-link"
+                    >当院について
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      fill="none"
+                    >
+                      <path
+                        d="M7.47002 14.3553C7.29126 14.3557 7.11803 14.2934 6.98038 14.1794C6.82392 14.0497 6.7255 13.863 6.70685 13.6606C6.68819 13.4583 6.75082 13.2568 6.88092 13.1006L10.3084 8.99995L7.00333 4.8916C6.87498 4.73355 6.81492 4.53085 6.83646 4.32838C6.858 4.12592 6.95936 3.94039 7.11809 3.81288C7.27812 3.67208 7.48962 3.60442 7.70166 3.6262C7.91369 3.64799 8.10702 3.75724 8.23507 3.92763L11.9303 8.51797C12.1623 8.80028 12.1623 9.20728 11.9303 9.48959L8.10501 14.0799C7.94935 14.2677 7.71348 14.37 7.47002 14.3553Z"
+                        fill="#1391E6"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+
+              <div class="c-sidebar__article">
+                <div class="c-sidebar__heading">
+                  <img
+                    src="img/blog/sidebaer/pc/blog-sidebaer-pc-04.svg"
+                    alt=""
+                  />
+                  <h2 class="c-sidebar__heading-title">新着記事</h2>
+                </div>
+                <div class="c-sidebar__article-cards">
+                  <a href="" class="c-sidebar__article-card">
+                    <div class="c-sidebar__article-image">
+                      <img
+                        src="img/blog/sidebaer/pc/blog-sidebaer-pc-01.webp"
+                        alt=""
+                      />
+                    </div>
+                    <div class="c-sidebar__article-content">
+                      <span class="c-sidebar__article-category">お知らせ</span>
+                      <p class="c-sidebar__article-text">
+                        記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                      </p>
+                      <time
+                        datetime="2020-02-14"
+                        class="c-sidebar__article-date"
+                        >2020.02.14</time
+                      >
+                    </div>
+                  </a>
+                  <a href="" class="c-sidebar__article-card">
+                    <div class="c-sidebar__article-image">
+                      <img
+                        src="img/blog/sidebaer/pc/blog-sidebaer-pc-01.webp"
+                        alt=""
+                      />
+                    </div>
+                    <div class="c-sidebar__article-content">
+                      <span class="c-sidebar__article-category">お知らせ</span>
+                      <p class="c-sidebar__article-text">
+                        記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                      </p>
+                      <time
+                        datetime="2020-02-14"
+                        class="c-sidebar__article-date"
+                        >2020.02.14</time
+                      >
+                    </div>
+                  </a>
+                  <a href="" class="c-sidebar__article-card">
+                    <div class="c-sidebar__article-image">
+                      <img
+                        src="img/blog/sidebaer/pc/blog-sidebaer-pc-01.webp"
+                        alt=""
+                      />
+                    </div>
+                    <div class="c-sidebar__article-content">
+                      <span class="c-sidebar__article-category">お知らせ</span>
+                      <p class="c-sidebar__article-text">
+                        記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                      </p>
+                      <time
+                        datetime="2020-02-14"
+                        class="c-sidebar__article-date"
+                        >2020.02.14</time
+                      >
+                    </div>
+                  </a>
+                  <a href="" class="c-sidebar__article-card">
+                    <div class="c-sidebar__article-image">
+                      <img
+                        src="img/blog/sidebaer/pc/blog-sidebaer-pc-01.webp"
+                        alt=""
+                      />
+                    </div>
+                    <div class="c-sidebar__article-content">
+                      <span class="c-sidebar__article-category">お知らせ</span>
+                      <p class="c-sidebar__article-text">
+                        記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                      </p>
+                      <time
+                        datetime="2020-02-14"
+                        class="c-sidebar__article-date"
+                        >2020.02.14</time
+                      >
+                    </div>
+                  </a>
+                  <a href="" class="c-sidebar__article-card">
+                    <div class="c-sidebar__article-image">
+                      <img
+                        src="img/blog/sidebaer/pc/blog-sidebaer-pc-01.webp"
+                        alt=""
+                      />
+                    </div>
+                    <div class="c-sidebar__article-content">
+                      <span class="c-sidebar__article-category">お知らせ</span>
+                      <p class="c-sidebar__article-text">
+                        記事のタイトルが入ります。記事のタイトルが入ります。記事のタイトルが入ります。
+                      </p>
+                      <time
+                        datetime="2020-02-14"
+                        class="c-sidebar__article-date"
+                        >2020.02.14</time
+                      >
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div class="c-sidebar__category">
+                <div class="c-sidebar__heading">
+                  <img
+                    src="img/blog/sidebaer/pc/blog-sidebaer-pc-05.svg"
+                    alt=""
+                  />
+                  <h2 class="c-sidebar__heading-title">カテゴリー</h2>
+                </div>
+                <ul class="c-sidebar__category-lists">
+                  <li class="c-sidebar__category-list">
+                    <a href="" class="c-sidebar__category-link"
+                      ><svg
+                        width="6"
+                        height="12"
+                        viewBox="0 0 6 12"
+                        fill="none"
+                      >
+                        <path d="M0 0L6 6L0 12V0Z" fill="#1391E6" /></svg
+                      >テキストテキスト</a
+                    >
+                  </li>
+                  <li class="c-sidebar__category-list">
+                    <a href="" class="c-sidebar__category-link"
+                      ><svg
+                        width="6"
+                        height="12"
+                        viewBox="0 0 6 12"
+                        fill="none"
+                      >
+                        <path d="M0 0L6 6L0 12V0Z" fill="#1391E6" /></svg
+                      >テキストテキスト</a
+                    >
+                  </li>
+                  <li class="c-sidebar__category-list">
+                    <a href="" class="c-sidebar__category-link"
+                      ><svg
+                        width="6"
+                        height="12"
+                        viewBox="0 0 6 12"
+                        fill="none"
+                      >
+                        <path d="M0 0L6 6L0 12V0Z" fill="#1391E6" /></svg
+                      >テキストテキスト</a
+                    >
+                  </li>
+                  <li class="c-sidebar__category-list">
+                    <a href="" class="c-sidebar__category-link"
+                      ><svg
+                        width="6"
+                        height="12"
+                        viewBox="0 0 6 12"
+                        fill="none"
+                      >
+                        <path d="M0 0L6 6L0 12V0Z" fill="#1391E6" /></svg
+                      >テキストテキスト</a
+                    >
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </aside>
